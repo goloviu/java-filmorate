bf5cfe3a335b9680a677bef9804e129a6f3959cf
Dmitry <golovin.goloviu@yandex.ru>
2024-04-12 21:21:08 +0300

    fix: убрать IGNORE в data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT IGNORE INTO rating (name)
+INSERT INTO rating (name)
 VALUES ('G'), ('PG'), ('PG-13'), ('R'), ('NC-17');
 
-INSERT IGNORE INTO genre (name)
+INSERT INTO genre (name)
 VALUES ('Комедия'), ('Драма'), ('Мультфильм'), ('Триллер'), ('Документальный'), ('Боевик');
